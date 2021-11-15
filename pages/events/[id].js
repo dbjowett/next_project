52cfbe90feaf3209a96f8131ca4a694a984cfd52
Daniel Jowett <dbjowett@gmail.com>
2021-11-15 14:46:23 +0900

    Add individual landing pages

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,21 +1,27 @@
 import { useRouter } from 'next/router';
-import Link from 'next/link';
+
 import { getEventById } from '../../dummy-data';
+import { Fragment } from 'react';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
 
 export default function DetailPage() {
   const router = useRouter();
   const currentEvent = getEventById(router.query.id);
   console.log(currentEvent);
+
+  if (!currentEvent) {
+    return <div> No event found!</div>;
+  }
+
   return (
-    <div>
-      <h1>Single Event Page</h1>
-      <div>Here we will show a selected event.</div>
-      <button>
-        <Link href={'/'}>Featured Events</Link>
-      </button>
-      <button>
-        <Link href={'/events/'}>All Events</Link>
-      </button>
-    </div>
+    <Fragment>
+      <EventSummary title={currentEvent.title} />
+      <EventLogistics date={currentEvent.date} address={currentEvent.location} image={currentEvent.image} imageAlt={currentEvent.title} />
+      <EventContent>
+        <p>{currentEvent.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
