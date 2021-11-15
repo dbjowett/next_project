516ba1a6512cbd531a2de41bdb9f4ab50a877982
Daniel Jowett <dbjowett@gmail.com>
2021-11-16 07:30:48 +0900

    Add search

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,10 +2,17 @@ import Link from 'next/link';
 import classes from './button.module.css';
 
 export default function Button(props) {
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        {/* Add own <a> tag to add classes/// Dont add href here tho */}
+        <a className={classes.btn}>{props.children}</a>
+      </Link>
+    );
+  }
   return (
-    <Link href={props.link}>
-      {/* Add own <a> tag to add classes/// Dont add href here tho */}
-      <a className={classes.btn}>{props.children}</a>
-    </Link>
+    <button classesName={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
   );
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,22 @@
-import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+import EventSearch from '../../components/events/events-search';
+import { Fragment } from 'react';
 
 export default function EventsPage() {
+  // Fire function and store it in Events
+  const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    router.push(`/events/${year}/${month}`);
+  }
+
   return (
-    <div>
-      <h1>Events Page</h1>
-      <div>Here we will show all events.</div>
-      <button>
-        <Link href={'/'}>Featured Events</Link>
-      </button>
-    </div>
+    <Fragment>
+      <EventSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
   );
 }
